b4e47108689911c1140bef82c361666b9b06944f
omarNaifer12 <o.naifer7@gmail.com>
2024-07-15 14:57:12 +0100

    done multer function upload images

---
+++ Backend/middleware/multer.js
@@ -0,0 +1,14 @@
+const multer =require( 'multer');
+const path=require("path");
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  });
+  const uploadsPath = path.join(__dirname,'../uploads');
+
+  const upload =multer({storage:storage});
+  module.exports={uploadsPath,upload};
